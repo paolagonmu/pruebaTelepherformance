c3c9fe4d6f9d2898c88234d9eb945b7ea3cdf978
angie paola gonzalez <a.paolagonmu6@gmail.com>
2021-07-30 11:10:12 -0500

    Add files via upload

---
+++ parte1/script.js
@@ -0,0 +1,27 @@
+
+
+//codigo agregado
+
+function calcular(){
+ var fechain = document.getElementById("fecha").value;
+ var fechasplit = fechain.split("/");
+ if(fechasplit.length===3){
+  var fecha = fechasplit[2]+"-"+fechasplit[1]+"-"+fechasplit[0];
+ }else{
+  var fecha = fechasplit[0];
+ }
+ 
+ console.log(fecha);
+
+  
+        var startdate = new Date(fecha);
+         var enddate = new Date('2021-12-31');
+          console.log(actualDate);
+          
+          var diff = enddate.getTime() - startdate.getTime();
+            todaydate = Math.round(diff / (1000 * 60 * 60 * 24));
+            console.log("today is"+todaydate);
+          var  calculodate = 365 - todaydate;
+            document.getElementById("diastrans").innerHTML="dias transcurridos: "+calculodate;
+  
+}
